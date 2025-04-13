19b4ee26792fc16deaaf36a0a331ba661b854efe
LAPTOP-4MJ3JI3S\techb <techbrian51@gmail.com>
2025-04-12 22:43:24 -0400

    Added camera deocclusion and movement now influenced by direction camera is facing

--- NecroBug/Assets/Scripts/PlayerControl.cs
+++ NecroBug/Assets/Scripts/PlayerControl.cs
@@ -2,18 +2,20 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerControl : MonoBehaviour
 {
-    Vector2 move = new Vector2();
+    Vector2 moveInput = new Vector2();
     Rigidbody rb;
-    float friction = 0.95f;
-    float moveForce = 10f;
+    float friction = .5f;
+    float moveForce = 200f;
     public bool wingsPickedUp = false;
     private void FixedUpdate()
     {
+        Vector2 move = RotateMoveByCamera(moveInput);
+        //might want to add checks for isGrounded to have air friction
         if (move != Vector2.zero)
         {
             rb.AddForce(new Vector3(move.x, 0, move.y) * moveForce);
         }
-        else
+        //else
         {
             Vector3 v = rb.linearVelocity;
             v.x *= friction;
@@ -26,9 +28,28 @@ public class PlayerControl : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    void OnMove(InputValue inputValue)
+    void OnMove(InputValue inputValue) 
     {
-        move =inputValue.Get<Vector2>();
+        moveInput = inputValue.Get<Vector2>();
+    }
+    Vector2 RotateMoveByCamera(Vector2 moveInputT)
+    {
+        // Rotation Matrix using the main camera's forward vector
+        Vector3 camFor = Camera.main.transform.forward;
+        //swapped x and z and made x negative, should be a 90 degree rotation (I don't know guessed and checked)
+        //its necessary because "forward" is in +z not +x
+        Vector2 cam = new Vector2(camFor.z, -camFor.x);
+        Vector2 m = new Vector2();
+
+        float a=cam.x;
+        float b=-cam.y;
+        float c=cam.y;
+        float d=cam.x;
+        m.x = a*moveInputT.x+b*moveInputT.y;
+        m.y = c*moveInputT.x+d*moveInputT.y;
+        m.Normalize();
+        
+        return m;
     }
    
     void OnJump(InputValue value)

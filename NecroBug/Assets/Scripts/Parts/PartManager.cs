0919011428a27348d2137f0fc480e0902beb2d75
Brian Hagewiesche <techbrian51@gmail.com>
2025-04-15 14:18:37 -0400

    Added a Strategy pattern to Inputs to make it so we should only need 1 Player Input now

---
+++ NecroBug/Assets/Scripts/Parts/PartManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PartManager : MonoBehaviour
+{
+    
+    public GameObject errorText;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AddPart(GameObject part)
+    {
+
+    }
+}

f44104733a1ed6d97228fd60d320878f9d90257b
Turple <68204099+Purple-Turple@users.noreply.github.com>
2025-04-21 13:28:50 -0400

    Procedural Animation testing
    Created a new scene (AnimTesting)
    Added a leg piece asset imported from maya
    Added a new folder in prefabs for body parts
    Added a new script for leg animation controls
    Added 3 new "debug" materials, transparent RGB

---
+++ NecroBug/Assets/Scripts/AnimLegControl.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class AnimLegControl : MonoBehaviour
+{
+    [Header("Variables")]
+
+    public float upwardRaycast;
+    public float downwardRaycast;
+    public float distanceBeforeSnap;
+    public float moveTime;
+
+    private float distance = 1;
+    private bool movementCompleted = true;
+
+    [field: Header("References")]
+
+    public GameObject tipController;
+
+    void Update()
+    {
+        TrackDistance();
+        StickToGround();
+
+        // Visualize the ray in Scene view
+        Debug.DrawRay(transform.position + Vector3.up * upwardRaycast, Vector3.down * downwardRaycast, Color.red);
+    }
+
+    void StickToGround()
+    {
+        RaycastHit hit;
+        // Start the ray higher to avoid hitting the object itself
+        if (Physics.Raycast(transform.position + Vector3.up * upwardRaycast, Vector3.down, out hit, downwardRaycast))
+        {
+            transform.position = new Vector3(
+                transform.position.x,
+                hit.point.y,
+                transform.position.z
+            );
+            // Debug.Log("Raycast hit: " + hit.point);
+        }
+    }
+
+    void TrackDistance()
+    {
+        distance = Vector3.Distance(transform.position, tipController.transform.position);
+        if (distance > distanceBeforeSnap)
+        {
+            MoveLeg();
+        }
+    }
+
+    void MoveLeg()
+    {
+        tipController.transform.position = Vector3.Lerp(tipController.transform.position, transform.position, moveTime * Time.deltaTime);
+    }
+}

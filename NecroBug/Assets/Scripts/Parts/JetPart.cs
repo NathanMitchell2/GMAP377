34a61c832c0608c2414058dfc54f0d95304cf089
Brian Hagewiesche <techbrian51@gmail.com>
2025-05-16 06:48:34 -0400

    Adjusted Jet, dash is now determined by direction jet is placed, also removed jet beetle droping jets for now.

--- NecroBug/Assets/Scripts/Parts/JetPart.cs
+++ NecroBug/Assets/Scripts/Parts/JetPart.cs
@@ -12,8 +12,9 @@ public class JetPart : ModularBugPart
             Rigidbody rigid = parent.GetComponent<Rigidbody>();
             if(rigid)
             {
-                Vector3 camFor = Camera.main.transform.forward;
-                rigid.linearVelocity = new Vector3(camFor.x*dashForce, rigid.linearVelocity.y, camFor.z*dashForce);
+                
+                Vector3 dashDir = transform.right;
+                rigid.linearVelocity = new Vector3(dashDir.x*dashForce, rigid.linearVelocity.y, dashDir.z*dashForce);
                 //rigid.linearVelocity = new Vector3(rigid.linearVelocity.x, jumpForce, rigid.linearVelocity.y);
             }
         }

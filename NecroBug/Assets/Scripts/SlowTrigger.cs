428ed446681fb6243e840b5f727164fc817b2ab2
Brian Hagewiesche <techbrian51@gmail.com>
2025-05-20 12:08:40 -0400

    Added Part Weight and some web script

---
+++ NecroBug/Assets/Scripts/SlowTrigger.cs
@@ -0,0 +1,16 @@
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class SlowTrigger : MonoBehaviour
+{
+    public float speedReduce = .5f;
+    public float steerReduce = .5f;
+    private void OnTriggerStay(Collider other)
+    {
+        carControler car = other.gameObject.GetComponent<carControler>();
+        if (car != null)
+        {
+            car.TerrainSpeedDown(speedReduce, steerReduce);
+        }
+    }
+}

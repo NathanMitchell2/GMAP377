a4c73ab7d90267d5be979058be8bb5276b77b9cb
Ben Muller <benlmuller2003@gmail.com>
2025-05-23 10:10:36 -0400

    Sound commit 1

--- NecroBug/Assets/Scripts/EnemyHealth.cs
+++ NecroBug/Assets/Scripts/EnemyHealth.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
+using FMODUnity;
 
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] protected int health = 100;
     [SerializeField] private HitFlash hitFlash;
 
+    [SerializeField]
+    private EventReference damageSound;
+
     public void dealDamage(int dmg)
     {
         health -= dmg;
         hitFlash.TriggerFlash();
+
+        if(damageSound.IsNull == false)
+        {
+            RuntimeManager.PlayOneShot(damageSound, transform.position);
+        }
         checkDeath();
     }
     private void checkDeath()

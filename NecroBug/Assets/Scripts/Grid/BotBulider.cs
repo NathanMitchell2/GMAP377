cbb39296e115af404aaf83df57368452be8ac0c4
Brian Hagewiesche <techbrian51@gmail.com>
2025-05-25 18:13:01 -0400

    Default Keybinds, (no longer is everything bound to 1)

--- NecroBug/Assets/Scripts/Grid/BotBulider.cs
+++ NecroBug/Assets/Scripts/Grid/BotBulider.cs
@@ -74,7 +74,7 @@ public class BotBulider : MonoBehaviour
     {
         part.Place(grid);
         //Instantiate(part.gameObject, gridTransform);
-        actionManager.AddAction();
+        actionManager.AddAction(part.defaultBind);
         parts.Add(part);
     }
     public bool MovePart(BotPart part, Vector3 pos)

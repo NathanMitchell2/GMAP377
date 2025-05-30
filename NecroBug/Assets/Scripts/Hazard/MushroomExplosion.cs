0ac19a0b9ccc98668e4946c1d70a8d24728ba0cf
Ben Muller <benlmuller2003@gmail.com>
2025-05-30 10:32:01 -0400

    week 9 sounds real

--- NecroBug/Assets/Scripts/Hazard/MushroomExplosion.cs
+++ NecroBug/Assets/Scripts/Hazard/MushroomExplosion.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
-
+using FMODUnity;
 public class MushroomExplosion : MonoBehaviour
 {
+    [SerializeField]
+    private string explosionSound = "event:/Other/mushroom explosion";
     public GameObject explosion;
     public float explosionForce = 5000f;
     public float explosionRadius = 5f;
@@ -10,6 +12,8 @@ public class MushroomExplosion : MonoBehaviour
         if(other.gameObject.tag == "Enemy" || other.gameObject.tag == "Player")
         {
             Instantiate(explosion, transform.position, Quaternion.identity);
+            RuntimeManager.PlayOneShot(explosionSound, Camera.main.transform.position);
+            Debug.Log("bomb sound played");
 
             Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
             foreach (Collider hit in colliders)

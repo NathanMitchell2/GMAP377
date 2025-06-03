083e68220462b302c4faa741dee54e3f11021e52
Turple <68204099+Purple-Turple@users.noreply.github.com>
2025-06-03 18:00:42 -0400

    Acid beetle projectile functionality

--- NecroBug/Assets/Scripts/AcidProjectile.cs
+++ NecroBug/Assets/Scripts/AcidProjectile.cs
@@ -5,16 +5,25 @@ public class AcidProjectile : MonoBehaviour
     public int damage = 10;
     public float lifeTime = 5f;
     public PlayerStats playerStats;
+    public ParticleSystem particle;
+    public EnemyAI acidBeetle;
 
     private void Start() => Destroy(gameObject, lifeTime);
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision other)
     {
-        if (other.CompareTag("Enemy")) return;
-        else if (other.CompareTag("Player"))
+        if (other.gameObject.tag == "Enemy") return;
+        else if (other.gameObject.tag == "Player")
         {
-            playerStats.TakeDamage(damage);
+            // Debug.Log("Damage");
+            PlayerStats playerStats = other.gameObject.GetComponentInParent<PlayerStats>();
+            acidBeetle.DealAcidDamage(damage, playerStats);
         }
+        // DOES NOT DESTROY THE PARTICLE, JUST STOPS IT. Persists forever in the scene, removing parent necessary for animation
+        particle.Stop();
+        particle.transform.parent = null;
+        // Destroys the acid prefab, not the particle
+        // Debug.Log("collision");
         Destroy(gameObject);
     }
 }

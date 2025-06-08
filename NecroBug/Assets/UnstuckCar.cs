7803f8bc9dad483e08a523e790e2e949df259962
Brian Hagewiesche <techbrian51@gmail.com>
2025-06-08 18:20:42 -0400

    Added Unstuck, ready for PR

---
+++ NecroBug/Assets/UnstuckCar.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UnstuckCar : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnUnstuck(InputValue value)
+    {
+
+        GameObject car = transform.GetChild(0).gameObject;
+        car.transform.SetLocalPositionAndRotation(car.transform.localPosition + Vector3.up, Quaternion.identity);
+    }
+}

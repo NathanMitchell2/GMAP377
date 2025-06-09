0a532f2b4194c7256209cd602bd5759fd6e8863b
Teimurazi Bakuradze <168293265+CDUCK04@users.noreply.github.com>
2025-06-08 21:37:29 -0400

    damage multiplier

--- NecroBug/Assets/Scripts/NewAttackSystem/HornDamageMultiplier.cs
+++ NecroBug/Assets/Scripts/NewAttackSystem/HornDamageMultiplier.cs
@@ -1,6 +1,40 @@
+using NUnit.Framework.Interfaces;
+using System.IO;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class HornDamageMultiplier : MonoBehaviour
 {
-    
+    private Rigidbody rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody otherRb = collision.rigidbody;
+
+        if (otherRb != null)
+        {
+            Vector3 relativeVelocity = collision.relativeVelocity;
+            float mass = rb.mass;
+
+            // Optional: Check if the collision was with the horn part
+            float impactMagnitude = mass * relativeVelocity.magnitude;
+            int damage = (int)Mathf.Round(impactMagnitude / 100);
+            Debug.Log("Damage: " + damage);
+          /* if (impactMagnitude > 10000)  
+               Destroy(gameObject);*/
+
+           
+
+          collision.transform.GetComponent<EnemyHealth>().dealDamage(damage);
+        }
+
+
+    }
+
 }

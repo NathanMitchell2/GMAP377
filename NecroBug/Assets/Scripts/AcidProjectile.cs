c475c0b99b1d63d334af2240daffbecdf88feb10
Brian Hagewiesche <techbrian51@gmail.com>
2025-06-10 20:34:51 -0400

    too many things, bug fixes and jet horn stuff mostly

--- NecroBug/Assets/Scripts/AcidProjectile.cs
+++ NecroBug/Assets/Scripts/AcidProjectile.cs
@@ -1,9 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class AcidProjectile : MonoBehaviour
 {
     public int damage = 10;
     public float lifeTime = 5f;
+    public float splashRange = .5f;
     public PlayerStats playerStats;
     public ParticleSystem particle;
     public GameObject splashEffect;
@@ -25,8 +27,20 @@ public class AcidProjectile : MonoBehaviour
         else if (other.gameObject.tag == "Player")
         {
             // Debug.Log(other.gameObject);
-            PlayerStats playerStats = other.gameObject.GetComponentInParent<PlayerStats>();
-            acidBeetle.DealAcidDamage(damage, playerStats);
+
+            List<GameObject> targets = new List<GameObject>();
+            Collider[] colliders = Physics.OverlapSphere(transform.position, splashRange);
+
+            foreach (Collider hit in colliders)
+            {
+                if (hit.gameObject.tag == "Player")
+                {
+                    targets.Add(hit.gameObject);
+                }
+            }
+            GetComponent<DamageObject>().Damage(DamageObject.GetPlayerHealths(targets));
+            //PlayerStats playerStats = other.gameObject.GetComponentInParent<PlayerStats>();
+            //acidBeetle.DealAcidDamage(damage, playerStats);
         }
         // DOES NOT DESTROY THE PARTICLE, JUST STOPS IT. Persists forever in the scene, removing parent necessary for animation
         

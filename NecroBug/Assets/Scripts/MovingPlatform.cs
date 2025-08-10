6a9d278433b3ba6417e78832fe2a1b1427ec364a
Jorp <g.joshua1206@gmail.com>
2025-08-10 18:44:37 -0400

    small comment
    small comment

--- NecroBug/Assets/Scripts/MovingPlatform.cs
+++ NecroBug/Assets/Scripts/MovingPlatform.cs
@@ -19,7 +19,7 @@ public class MovingPlatform : MonoBehaviour
         {
             if (mover != null)
             {
-                mover.AddPlatformDelta(delta);
+                //mover.AddPlatformDelta(delta);
             }
         }
 

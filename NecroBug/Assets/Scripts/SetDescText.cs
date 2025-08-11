c594afb01eb41720139b60868a7db5f0fbdeb5d0
NathanMitchell2 <nathan.m.mitchell2@gmail.com>
2025-08-11 17:42:49 -0400

    descriptions are now hidden until bug dies

--- NecroBug/Assets/Scripts/SetDescText.cs
+++ NecroBug/Assets/Scripts/SetDescText.cs
@@ -6,13 +6,38 @@ public class SetDescText : MonoBehaviour
 {
     public TextMeshProUGUI descriptionTextObj; 
     public string descriptionText;
+    public string enemyType;
 
     public TextMeshProUGUI dropsTextObj;
     public string dropsText;
     public void ChangeTextOnClick()
     {
-        descriptionTextObj.text = descriptionText;
-        dropsTextObj.text = "Drops:\n"+dropsText;
+        if (showDesc()){
+            descriptionTextObj.text = descriptionText;
+            dropsTextObj.text = "Drops:\n"+dropsText;
+        }
+        else {
+            descriptionTextObj.text = "?????";
+            dropsTextObj.text = "Drops:\n??????";
+        }
+    }
+
+    private bool showDesc(){
+        if(enemyType == "jetBeetle" && BugopediaEvents.jetBeetleKilled == true){
+            return true;
+        }
+        else if(enemyType == "acidBeetle" && BugopediaEvents.acidBeetleKilled == true){
+            return true;
+        }
+        else if (enemyType == "spider" && BugopediaEvents.spiderKilled == true){
+            return true;
+        }
+        else if (enemyType== "bee" && BugopediaEvents.beeKilled == true){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
 }

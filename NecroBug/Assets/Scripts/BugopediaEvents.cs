664876aa9e86c53c487bbd7e326944fecf4bd343
NathanMitchell2 <nathan.m.mitchell2@gmail.com>
2025-09-02 20:23:05 -0400

    forgot to undo my testing vars

--- NecroBug/Assets/Scripts/BugopediaEvents.cs
+++ NecroBug/Assets/Scripts/BugopediaEvents.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class BugopediaEvents : MonoBehaviour
 {
-    /*
     static public bool jetBeetleKilled = false;
     static public bool acidBeetleKilled = false;
     static public bool orbWeaverKilled = false;
@@ -10,12 +9,4 @@ public class BugopediaEvents : MonoBehaviour
     static public bool frenepedeDiscovered = false;
     static public bool beeKilled = false;
     static public bool queenKilled = false;
-    */
-    static public bool jetBeetleKilled = true;
-    static public bool acidBeetleKilled = true;
-    static public bool orbWeaverKilled = true;
-    static public bool laserSpiderKilled = true;
-    static public bool frenepedeDiscovered = true;
-    static public bool beeKilled = true;
-    static public bool queenKilled = true;
 }
